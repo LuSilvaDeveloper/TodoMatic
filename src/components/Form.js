8f68fc2f52dc31f9209d80a756419ed795b366f3
LuSilvaDeveloper <lusilva.dev@icloud.com>
2022-10-02 17:12:24 -0700

    Clear Tasks done / Toggle Button to switch Theme Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,48 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from "react";
 
 function Form(props) {
     const [name, setName] = useState("");
+    const [isClear, setClear] = useState(false);
 
     function handleSubmit(e) {
-      e.preventDefault();
-      props.addTask(name);
-      setName("");
-    }   
-
-    function handleChange(e) {
-      setName(e.target.value);
+        e.preventDefault();
+        props.addTask(name);
+        setName("");
     }
 
-    return(
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
+
+    return (
         <form onSubmit={handleSubmit}>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          value={name}
-          onChange={handleChange}
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
+            </h2>
+
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                value={name}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+            <button
+                type="button"
+                className="btn btn__secondary btn__lg"
+                onClick={() => props.handleClear()}
+            >
+                Clear All Tasks
+            </button>
+        </form>
     );
 }
 
